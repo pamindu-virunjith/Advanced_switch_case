740d7615c4a8a53402c704047e51c041af82d4d4
hp <hp@DESKTOP-QPR8JLM>
2024-11-09 10:21:47 +0530

    Add project files.

---
+++ Advanced_switch_case/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_switch_case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sku = "01-MN-L";
+
+            string[] product = sku.Split('-');
+
+            string type = "";
+            string color = "";
+            string size = "";
+
+            switch (product[0])
+            {
+                case "01":
+                    type = "Sweat shirt";
+                    break;
+                case "02":
+                    type = "T-Shirt";
+                    break ;
+            }
+
+            Console.WriteLine($"the product type: {type}");
+        }
+    }
+}
